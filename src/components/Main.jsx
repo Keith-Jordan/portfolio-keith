f99a9901c092926c853f4dd4631b37002f691335
Keith Jordan <keithyjordan11@gmail.com>
2024-03-08 17:00:07 -0600

    changed work history

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,56 +1,64 @@
 import React from "react";
-import {TypeAnimation} from 'react-type-animation';
-import {FaLinkedin, FaGithub} from 'react-icons/fa';
+import { TypeAnimation } from "react-type-animation";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Main = () => {
-    return (
-        <div id='main'>
-            <img className='w-full h-screen object-cover object-left' 
-            src="https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?cs=srgb&dl=cold-snow-dawn-618833.jpg&fm=jpg"
-            alt='/'
-             />
-            <div className='w-full h-screen absolute top-0 left-0 bg-white/35'>
-                <div className='mx-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center'>
-                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Keith Jordan</h1>
-                    <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800 '>I'm a     
-                    <TypeAnimation
-                     sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                           'Front End Developer',
-                         2000, 
-                            'Coder',
-                             2000,
-                            'Tech Enthusiast',
-                          2000,
-                            'Student',
-                             2000
-                             ]}
-                     wrapper="div"
-                     speed={50}
-                      style={{ fontSize: '1em', display: 'inline-block', paddingLeft: '5px' }}
-                      repeat={Infinity}
-                     />
-                    </h2>
-                    <div className='flex justify-between pt-6 max-w-[200px] w-full'>
-                     <a 
-                        href="https://www.linkedin.com/in/keith-jordan-a98114275"
-                        target="_blank"
-                        rel="noopener noreferrer"  
-                    >
-                        <FaLinkedin className='cursor-pointer' size={20} />
-                    </a>
-                    <a 
-                        href="https://github.com/Keith-Jordan"  // Update with your GitHub profile URL
-                        target="_blank"
-                        rel="noopener noreferrer" 
-                    >
-                        <FaGithub className='cursor-pointer' size={20} /> 
-                    </a>
-                </div>
-            </div>
-            </div>
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left"
+        src="https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?cs=srgb&dl=cold-snow-dawn-618833.jpg&fm=jpg"
+        alt="/"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/35">
+        <div className="mx-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Keith Jordan
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800 ">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Front End Developer",
+                2000,
+                "Coder",
+                2000,
+                "Tech Enthusiast",
+                2000,
+                "Student",
+                2000,
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{
+                fontSize: "1em",
+                display: "inline-block",
+                paddingLeft: "5px",
+              }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <a
+              href="https://www.linkedin.com/in/keith-jordan-a98114275"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="cursor-pointer" size={20} />
+            </a>
+            <a
+              href="https://github.com/Keith-Jordan" // Update with your GitHub profile URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="cursor-pointer" size={20} />
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,28 +3,28 @@ import WorkItem from './WorkItem';
 
 const data = [
     {
-        year: 2020,
-        title: 'something',
-        duration: '1 year',
-        details: 'did stuff'
+        year: 2023,
+        title: 'Started Learning Front End Development',
+        duration: '1 year and counting',
+        details: 'Embarking on the journey to master front-end development, and learn as much as I can!'
+    },
+    {
+        year: 2021,
+        title: 'Premium Waters',
+        duration: '3 years',
+        details: 'Currently working at Premium Waters Inc. as an Inventory Control Clerk.'
     },
     {
         year: 2020,
-        title: 'something',
+        title: 'Heritage Auctions',
         duration: '1 year',
-        details: 'did stuff'
+        details: 'Worked at Heritage Acutions House in the comics and animation department.'
     },
     {
-        year: 2020,
-        title: 'something',
-        duration: '1 year',
-        details: 'did stuff'
-    },
-    {
-        year: 2020,
-        title: 'something',
-        duration: '1 year',
-        details: 'did stuff'
+        year: 2017,
+        title: 'Del Friscos Grille',
+        duration: '3 years',
+        details: 'Worked as a waiter at Del Friscos'
     },
 ]
 const Work = () => {
